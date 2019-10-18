56db21e421fe0e869d9fb84ca02c919cad31a9a6
PCWitcher <pcwitcher.ie@gmail.com>
2019-10-18 12:18:39 +0100

    array lists App

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
-import Column from '../Column/Column';
+//import Column from '../Column/Column';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator';
+//import Creator from '../Creator/Creator';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  };
 
   static propTypes = {
     title: PropTypes.node.isRequired,
@@ -23,35 +20,25 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   };
 
-  addColumn(title) {
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length - 1].key + 1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
 
   render() {
+
+    const { title, image, description } = this.props;
+
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imageURL={this.props.image} />
-        <div className={styles.description}>{ReactHtmlParser(this.props.description)}</div>
-        <div className={styles.columns}>
+        <Hero titleText={title} image={image} />
+        <div className={styles.description}>
+          {ReactHtmlParser(description)}
+        </div>
+        {/*<div className={styles.columns}>
           {this.state.columns.map(({ key, ...columnProps }) => (
             <Column key={key} {...columnProps} />
           ))}
         </div>
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
-        </div>
+          </div>*/}
       </section>
     );
   }
